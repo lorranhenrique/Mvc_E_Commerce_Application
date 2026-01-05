3af64b0fa784189354d85879eb6c8a228439d4af
Lorran <logplayviews@gmail.com>
2026-01-05 17:26:34 -0300

    Feat: login requirement for a payment view

--- E_Commerce/routes/pagamentoRoutes.js
+++ E_Commerce/routes/pagamentoRoutes.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var pagamentoController = require("../controllers/pagamentoController");
+var {requireAuth} =  require("../middlewares/authMiddleware");
 
-router.post('/', pagamentoController.pagamentoIndex);
-router.post('/efetuarPagamento', pagamentoController.efetuarPagamento);
+router.post('/',requireAuth, pagamentoController.pagamentoIndex);
+router.post('/efetuarPagamento', requireAuth, pagamentoController.efetuarPagamento);
 
 module.exports = router;
